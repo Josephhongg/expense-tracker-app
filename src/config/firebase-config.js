85fc10a54709b1fd43a9fb3ee13dfca0675a3754
josephhongg <joseph.hong99@gmail.com>
2023-12-22 19:35:45 +1300

    setup firebase and routes

---
+++ src/config/firebase-config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArTawVSdK3-lpPY21A-PMgR8D43__96lg",
+  authDomain: "fir-tutorial-cb6e3.firebaseapp.com",
+  projectId: "fir-tutorial-cb6e3",
+  storageBucket: "fir-tutorial-cb6e3.appspot.com",
+  messagingSenderId: "732004196982",
+  appId: "1:732004196982:web:bfa0be2bad737b8e7a7557",
+  measurementId: "G-TV0ZGVJYPG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
