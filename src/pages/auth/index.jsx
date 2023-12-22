53ff311794bd5091a565d91629330d84dc81667d
josephhongg <joseph.hong99@gmail.com>
2023-12-22 19:53:15 +1300

    set up Auth Component

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,3 +1,23 @@
+import { auth, provider } from "../../config/firebase-config";
+import { signInWithPopup } from "firebase/auth"
+
 export const Auth = () => {
-    return <div>Hello World Auth</div>
-}
+    const signInWithGoogle = async () => {
+       const results = await signInWithPopup(auth, provider);
+       const authInfo = {
+        userID: results.user.uid,
+        name: results.user.displayName,
+        profilePhoto: results.user.photoURL,
+        isAuth: true,
+       }
+       localStorage.setItem("auth", JSON.stringify(authInfo));
+    }
+	return (
+    <div className="login-page">
+        <p>Sign In With Google to Continue</p>
+        <button className="login-with-google-btn" onClick={signInWithGoogle}>
+            Sign In With Google
+        </button>
+    </div>
+    )
+};
